c1285bc288c334d8aef4c1c2003bc49e944758d3
iosdev747 <iosdev747@icloud.com>
2018-10-09 12:12:19 +0530

    Add Network manager, server interface and game

---
+++ src/sample/ActionID.java
@@ -0,0 +1,44 @@
+package sample;
+
+// **************************************************
+// Fields
+// **************************************************
+
+/**
+ * Class ActionID stores all action codes that helps
+ * server to perform specific action according to request
+ */
+public class ActionID {
+    static final String search_ID = "0";
+    static final String createUser_ID = "1";
+    static final String getUserInfo_ID = "2";
+    static final String checkCredentials_ID = "3";
+    static final String createMerchant_ID = "4";
+    static final String getMerchantInfo_ID = "5";
+    //    static final String isMerchant_ID = "6";
+    static final String addProduct_ID = "7";
+    static final String getMerchantsProduct_ID = "8";
+    static final String removeProduct_ID = "9";
+    static final String showProductOfCategory_ID = "10";
+    static final String getCategoty_ID = "11";
+    static final String getSubCategoty_ID = "12";
+    static final String getProductBetween_ID = "13";
+    static final String getRange_ID = "14";
+    static final String purchaseProduct_ID = "15";
+    static final String searchProductOrderByPrice_ID = "16";
+    static final String searchProductOrderByName_ID = "17";
+    static final String getProductOfCategoryOrderByPrice_ID = "18";
+    static final String getProductOfCategoryOrderByName_ID = "19";
+    static final String getProductOfSubCategory_ID = "20";
+    static final String getProductOfSubCategoryOrderByPrice_ID = "21";
+    static final String getProductOfSubCategoryOrderByName_ID = "22";
+    static final String getUserCart_ID = "23";
+    static final String getCartOfPriceBetween_ID = "24";
+    static final String getAllUserCart_ID = "25";
+    static final String getAllMerchantHistory_ID = "26";
+    static final String getMerchantHistoryOfUser_ID = "27";
+    static final String getMerchantHistoryOfProduct_ID = "28";
+    static final String getNotification_ID = "29";
+    static final String removeNotification_ID = "30";
+    static final String addtocart_ID = "31";
+}
